52bed923f5e9f26b68ed20fbad2dcf640a086c84
Sushil Gupta <kalwarsushil2@gmail.com>
2022-08-24 20:05:09 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,91 @@ import React from 'react';
 import {nanoid} from 'nanoid'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
-import MyTime from './components/MyTime'
+import { useStopwatch } from 'react-timer-hook'
 
 function App() {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  }= useStopwatch({ autoStart: true });
+
+  const [userStats, setUserStats] = React.useState(
+    () => JSON.parse(localStorage.getItem("userStats")) || []
+  )
+  
+  const [currentUserId, setCurrentUserID] = React.useState(
+    (userStats[0] && userStats[0].id) || ""
+  )
+
+  const [userName, setUsername] = React.useState("None")
+
+  const[isNewuser, setIsNewuser] = React.useState(false)
   const { width, height } = useWindowSize()
   const [dice, setDice] = React.useState(allNewDice())
   const [noRoll, setNoRoll] = React.useState(0)
   const [tenzies, setTenzies] = React.useState(false)
   
+  React.useEffect(() =>{
+    localStorage.setItem("useStats", JSON.stringify(...userStats,userStats))
+    
+  }, [userStats])
+
+  function createUser(){
+    const Newuser = {
+      id:nanoid(),
+      name:"",
+      totalRoll:0,
+      second:0,
+      minute:0
+    }
+    setUserStats(oldUser => [...oldUser, Newuser])
+    setCurrentUserID(Newuser.id)
+    setIsNewuser(true)
+  }
+
+  function updateUsername(event){
+    const {value} = event.target
+    setUsername(value) 
+  }
+
+  function update(){
+    
+    console.log(userStats)
+    
+  }
+
+  /*##################################################*/
 
   React.useEffect(() => {
     const allHeld = dice.every(die => die.isHeld)
     const firstValue = dice[0].value
     const allSameValue = dice.every(die => die.value === firstValue )
     if (allHeld && allSameValue){
-      setTenzies(true)
-    }
+      setTenzies(true) 
+      setUserStats(oldValues => {
+        const newArray = []
+        for (let i=0; i<oldValues.length; i++){
+          const oldValue = oldValues[i]
+          if(oldValue.id === currentUserId){
+            newArray.unshift({
+              ...oldValue,
+              name:userName,
+              totalRoll:noRoll,
+              second:seconds,
+              minute:minutes})
+            }else{
+              newArray.push(oldValue)
+            }
+          }
+          return newArray
+        })
+    }    
   }, [dice])  
 
   function generateNewDie(){
@@ -48,7 +117,8 @@ function App() {
     }else{
       setTenzies(false)
       setDice(allNewDice)
-      setNoRoll(0)
+      setNoRoll(0) 
+      setIsNewuser(false)
     }
     
   }
@@ -61,20 +131,22 @@ function App() {
     }))
   }
 
-  const [time, setTime] = React.useState({
+  
+
+  /*const [time, setTime] = React.useState({
     id:"",
     second:"0",
     minutes:"0"
   })
 
-  function recordTime(event, sec, min){
+  function recordTime(sec, min){
     console.log('inside record time')
     setTime({
       id:dice.id,
-      second:sec,
-      minutes:min
+      second:seconds,
+      minutes:minutes
     })
-  }
+  }*/
 
   const diceElements = dice.map(die => (
     <Die 
@@ -83,33 +155,66 @@ function App() {
         isHeld={die.isHeld} 
         holdDice={() => holdDice(die.id)}
     />
-))
+  ))
+
+  const score = userStats.map(items => (
+    <p className='final-text'>
+      {items.name && `${items.name} has rolled dice ${items.totalRoll} in ${items.minute}:${items.second} time`}
+    </p>
+  ))
 
   return (
     <div className="App">
-      <main>
-        <section className='text'>
-          <h1 className="title">Tenzies</h1>
-          <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-        </section>
-        <div className='dice-container'>
-          {diceElements}
-        </div>
-        <p>{time.second}</p>
-        <p>{tenzies ? "Total":"" } Number of dice rolled : {noRoll}</p>
-        {tenzies && <div className='game-won'>Congrats, You won the game</div> }
-        { tenzies && <Confetti
-                      width={width}
-                      height={height}
-                />}
-        <button className='roll-dice' onClick={rollDice}>
-          {tenzies ? "New Game" : "Roll"}</button>
-        
-      </main>
+          { tenzies && <Confetti
+                        width={width}
+                        height={height}
+                  />}
+          <main>
+            <section className='text'>
+              <h1 className="title">Tenzies</h1>
+              <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            </section>
+            {/* starting the game  */}
+            {
+              isNewuser ?
+                <div>
+                  <div className='dice-container'>
+                    {diceElements}
+                  </div>
+                  <div className='stats'>
+                    <p className='time'>Time : {minutes}:{seconds}</p>
+                    <p className='no-diceroll'>Number of times dice rolled : {noRoll}</p>
+                    {tenzies && <div className='game-won'>Congrats, You won the game</div> }
+                  </div>
+                  <button className='roll-dice' onClick={rollDice}>
+                    {tenzies ? "New Game" : "Roll"}</button>
+                  {/*<button onClick={update} className="roll-dice newuser-btn">Update</button>*/}
+                  <section className='score-section'>
+                    {score}
+                  </section>
+                </div>
+              :
+              
+                  <div className='welcome-main'>
+                    <h2 className='welcome-text'>Welcome to the game!</h2>
+                    <input className='input-username' 
+                          type="text"
+                          placeholder='Enter your name'
+                          name='username'
+                          value={userStats.name}
+                          onChange={updateUsername} />
+                    <br />
+                    <button onClick={createUser} className="roll-dice newuser-btn">Start</button>
+                    
+                  </div>
+            }
+          </main>
 
+      {/*
       <MyTime 
         setTime={setTime}
         recordTime={recordTime}/>
+      */}
 
 
     </div>
